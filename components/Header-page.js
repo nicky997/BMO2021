2b0e3bfe376bf566019867b619ed0a1c85c6a197
nicky997 <nicky_997@mail.ru>
2021-05-21 01:35:30 +0300

    Adding new files

---
+++ components/Header-page.js
@@ -0,0 +1,26 @@
+import Nav from '../components/Nav'
+const Header_page = () => {
+    return(
+        <div className={"header-wrapper"}>
+            <div className={"header-menu"}>
+                <div className={"jbmo-logo"}>JBMO | </div>
+                <Nav/>
+            </div>
+
+                <div className="header-info-page">
+            <div className={"welcome-text"}>
+
+                <h1><span>JBMO 2021 </span></h1>
+                <h2><span>25th Junior Balkan Mathematical Olympiad </span></h2>
+                <h3><span>June 23-29, 2021, Chişinău, Republic of Moldova</span></h3>
+
+
+            </div>
+
+            </div>
+
+
+        </div>
+    )
+}
+export default Header_page
